904ed73d024abd822b7af7ca7d276657144d7457
JuanEstebanLopez1977 <41590838+JuanEstebanLopez1977@users.noreply.github.com>
2018-08-22 17:23:22 -0300

    Delete EmployeeRepositoryCustom.java

--- SpringBootandMongoDB/src/main/java/com/bgba/EmployeeRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.bgba;
-
-import java.util.Date;
-
-public interface EmployeeRepositoryCustom {
-	public long getMaxEmpId();
-
-	public long updateEmployee(String empNo, String fullName, Date hireDate);
-}
